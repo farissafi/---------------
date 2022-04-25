15f89de5af73d58b981ab38b439ad47ee6bd7aae
faris_safi <f409682762@gmail.com>
2022-04-25 17:42:34 +0300

    اجازات الموظفين

---
+++ els/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | للمزيد من المشاريع  |<a href="https://tech-code24.net/">اضغط هنا</a></p>
+        </div>
+</footer>
